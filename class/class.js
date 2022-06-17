f1ea4973cd47144116630f8c3b3e69f443afd390
haribabuN21 <haribabun119@gmail.com>
2022-06-17 18:52:22 +0530

    code

---
+++ class/class.js
@@ -0,0 +1,9 @@
+let vikram ={
+    hero:"kamal",
+    villan:"vijaysethupathi",
+    mainVillan:"surya",
+    stroryPlot:"vikram is a raw agent and his son is killed ",
+    director:lokesh,
+    upcommingmovies:["kaithi 2","vikram 2","vikram 3",
+]
+}
