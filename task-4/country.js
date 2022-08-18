8b737df9b7721ae6a11421ef38bfee41e1cc7d38
haribabuN21 <haribabun119@gmail.com>
2022-08-18 10:13:53 +0530

    task-4

---
+++ task-4/country.js
@@ -0,0 +1,31 @@
+ //4 steps
+
+ //create XHR Object 
+
+ var request  =new XMLHttpRequest();
+
+ request.open("GET" ,'https://restcountries.com/v3.1/all',true);
+ 
+ //request send
+ 
+ request.send();
+ 
+ //4.recivie a respon
+ request.onload = function(){
+     if(request.status == 200){
+         var data = JSON.parse(request.responseText);
+         for(i=0;i<data.length;i++){
+              console.log("name= "+data[i].name.official);
+              console.log("reg= "+data[i].region);
+              console.log("sr= "+data[i].subregion);
+              console.log("people= "+data[i].population);
+
+              
+                      
+         }
+     }
+ }
+ 
+ 
+ 
+ 
